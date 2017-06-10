7cd41a7e8a79802a87db70ec6715629845fd5f40
Johannes Drechsler <jonjohnsonwright@gmail.com>
2017-06-10 19:05:15 +0200

    new format

--- CreateComponent.ts
+++ CreateComponent.ts
@@ -1,28 +1,80 @@
-// var args: string[] = ["C:\node\node.exe", "C:\Users\DREC006.ASYSOFFICE\Documents\NodeJS\CreateComponent\CreateComponent.ts"]
-// args.push("cc")
-// args.push("cCsdZT")
+import * as fs from 'fs'
+import * as fsa from 'async-file'
+import * as ncp from 'ncp'
 
-var args = process.argv /*?*/
-if (args.length != 4) {
-	console.log("Please check your params. Correct would be: cc compName.")
-	process.exit(0)
+var isTestMode: boolean = false
+
+var args: string[]
+
+if (isTestMode) {
+	console.log("---Testmode---")
+	args = ["C:\node\node.exe", "C:\Users\DREC006.ASYSOFFICE\Documents\NodeJS\CreateComponent\CreateComponent.ts"]
+	args.push("cc")
+	args.push("cCsdZT")
+} else {
+	console.log("---Normal Mode---")
+	args = process.argv /*?*/
+	if (args.length < 4 || args.length > 4) {
+		console.log('Please correct your params.')
+		process.exit(1)
+	}
 }
-
 
 var scriptCall: string = args[2] /*?*/
 var componentName: string = args[3] /*?*/
 var regCheckCompName: RegExp = /^[a-zA-Z]+$/g
+var regCheckScriptCall: RegExp = /^cc$/g
 
-componentName.match(regCheckCompName) /*?*/
-
-if (scriptCall === 'cc' && componentName.match(regCheckCompName)) {
-	console.log("compName correct.")
-} else {
-	console.log("Please only use compName in this format a-Z.")
-	process.exit(0)
+if (scriptCall.match(regCheckScriptCall)/*?*/) {
+	console.log("Scriptaufruf korrekt.")
+	if (componentName.match(regCheckCompName)/*?*/) {
+		console.log("compName correct.")
+		createComponent(componentName)
+	} else {
+		console.log("Please only use compName in this format a-Z.")
+	}
 }
 
-var componentLocation: string = `${__dirname}/${componentName}` /*?*/
+function createComponent(compName: string) {
+	var componentLocation: string = `${__dirname}/${componentName}` /*?*/
+	console.log(`Erstellt wird Component ${componentName} im Ordner ${componentLocation}.`)
+	var source: string = 'C:\\Template'
+	copyTemplate(source, componentLocation)
+	console.log('Script successfully ended.')
+}
 
-console.log(`Erstellt wird Component ${componentName} im Ordner ${componentLocation}.`)
-console.log('Script successfully ended.')
+function copyTemplate(source: string, destination: string) {
+	destination/*?*/
+	componentName/*?*/
+
+	ncp(source, destination, function (err) {
+		if (err) {
+			return console.error(err);
+		}
+		console.log('done!');
+		replaceTemplateStrings(destination, componentName)
+	});
+
+}
+
+async function replaceTemplateStrings(destination: string, compName: string) {
+	var files = await fsa.readdir(destination)
+	for (var index = 0; index < files.length; index++) {
+		var file = files[index]/*?*/
+		var filePath = `${destination}/${file}`
+		var newFileContent = await fsa.readFile(filePath, "utf8")/*?*/
+		newFileContent = newFileContent.replace(/Template/g, compName)/*?*/
+		fs.writeFileSync(filePath, newFileContent, 'utf8')
+		console.log(`Created: ${filePath}.`)
+	}
+}
+
+function fsExistsSync(myDir) {
+	try {
+		fs.accessSync(myDir);
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
